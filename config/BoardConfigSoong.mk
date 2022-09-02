e0e76b3d57e678063e22a289a7d6b6074728b6f0
html6405 <peter.schelchshorn@mhs-solutions.at>
2022-09-02 14:17:33 +0200

    Revert "config: Remove TARGET_DISABLE_POSTRENDER_CLEANUP"
    This reverts commit fa5bc45a95fae4cbd68b3d19265f5f1e799792e2.
    
    Change-Id: I5c4ba3231a1e225a0511177101cac0f9ffbd5c21

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -31,6 +31,7 @@ SOONG_CONFIG_lineageGlobalVars += \
     additional_gralloc_10_usage_bits \
     gralloc_handle_has_reserved_size \
     bootloader_message_offset \
+    disable_postrender_cleanup \
     needs_camera_boottime \
     target_init_vendor_lib \
     target_ld_shim_libs \
@@ -58,6 +59,7 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_lineageGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
+SOONG_CONFIG_lineageGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_lineageGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_lineageGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_lineageNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
